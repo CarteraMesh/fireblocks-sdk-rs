4bf851219ed1ca5f4387cd7ef7f6acedec29e81d
0xgleb <gleb.dianov@protonmail.com>
2026-03-05 01:40:01 +0700

    fix: Confirming is not a terminal transaction status

--- src/client/poll.rs
+++ src/client/poll.rs
@@ -23,21 +23,29 @@ impl Client {
         interval: std::time::Duration,
         callback: impl Fn(&TransactionResponse) + Send + Sync,
     ) -> crate::Result<TransactionResponse> {
+        use TransactionStatus::{
+            Blocked, Broadcasting, Cancelled, Cancelling, Completed, Confirming, Failed,
+            Pending3RdParty, Pending3RdPartyManualApproval, PendingAmlScreening,
+            PendingAuthorization, PendingEnrichment, PendingSignature, Queued, Rejected, Submitted,
+        };
+
         let mut total_time = Duration::from_millis(0);
         loop {
             if let Ok(result) = self.get_transaction(id).await {
                 let status = &result.status;
                 debug!("status {:#?}", status);
-                #[allow(clippy::match_same_arms)]
                 match status {
-                    TransactionStatus::Blocked => break,
-                    TransactionStatus::Cancelled => break,
-                    TransactionStatus::Cancelling => break,
-                    TransactionStatus::Completed => break,
-                    TransactionStatus::Confirming => break,
-                    TransactionStatus::Failed => break,
-                    TransactionStatus::Rejected => break,
-                    _ => {
+                    Blocked | Cancelled | Cancelling | Completed | Failed | Rejected => break,
+                    Submitted
+                    | PendingAmlScreening
+                    | PendingEnrichment
+                    | PendingAuthorization
+                    | Queued
+                    | PendingSignature
+                    | Pending3RdPartyManualApproval
+                    | Pending3RdParty
+                    | Broadcasting
+                    | Confirming => {
                         callback(&result);
                     }
                 }
